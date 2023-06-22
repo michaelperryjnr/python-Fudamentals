7f46c4f2cda16286406bd17eeefb3488988ede44
Michael Perry Tettey <125149768+michaelperryjnr@users.noreply.github.com>
2023-06-22 20:30:39 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash
+from sqlalchemy.orm.exc import NoResultFound  # Import the NoResultFound exception
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except NoResultFound:  # Handle NoResultFound exception
+        return None
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Rest of the code remains the same...
