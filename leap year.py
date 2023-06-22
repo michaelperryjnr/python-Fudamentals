f226271893ae63b1a6dec812d78a1a57f85da797
Michael Perry Tettey <125149768+michaelperryjnr@users.noreply.github.com>
2023-06-22 14:35:43 +0000

    Update leap year.py

--- leap year.py
+++ leap year.py
@@ -1,4 +1,6 @@
+#allow user to input year 
 year = int (input("Input a year\n"))
+#function to check the year by modulus 
 if (year % 4 == 0):
     print ("The Year ", year , "is a leap year")
 else:
