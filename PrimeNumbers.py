7a825f7f5a5f5c8c89f33175c8cec9094f9fe57c
Michael Perry Tettey <125149768+michaelperryjnr@users.noreply.github.com>
2023-06-22 14:32:33 +0000

    Update PrimeNumbers.py

--- PrimeNumbers.py
+++ PrimeNumbers.py
@@ -1,9 +1,13 @@
+#create an empty array for  prime numbers
 primeNumbers = []
+#code to generate numbers from 2 to 98
 for x in range(2,98):
+    #this code checks if the number is actually a primenumber
     for n in range(2,x):
         if x % n == 0:
             break
     else:
         primeNumbers.append(x)
+#the prime numbers are printed and summed with the code below
 print(primeNumbers)
 print("The sum of Prime Numbers are", sum(primeNumbers))
