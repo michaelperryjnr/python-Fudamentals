eaad0ca2add73997ffae6a8c49f8b72a2d93c440
Michael Perry Tettey <125149768+michaelperryjnr@users.noreply.github.com>
2023-06-22 14:25:13 +0000

    Update PrimeNumbers.py

--- PrimeNumbers.py
+++ PrimeNumbers.py
@@ -1,2 +1,9 @@
-while [int (x = 1), x<=100, x++]:
-    print("Hello World")
+primeNumbers = []
+for x in range(2,98):
+    for n in range(2,x):
+        if x % n == 0:
+            break
+    else:
+        primeNumbers.append(x)
+print(primeNumbers)
+print("The sum of Prime Numbers are", sum(primeNumbers))
