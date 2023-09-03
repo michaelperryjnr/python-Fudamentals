9aab5a81a1c65df5146585876bbb00e853e52e82
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-09-03 00:36:38 +0000

    final phase, work done

--- gpa_function.py
+++ gpa_function.py
@@ -14,11 +14,29 @@ def gpa_1 (score):
     else:
         ans = 0.0
         return ans
-course_num = input("Enter the number of courses you take\n")
+def gpa_2 (letter):
+    if letter >= 90:
+        ans = "A"
+        return ans
+    elif letter >= 80:
+        ans = "B"
+        return ans
+    elif letter >= 70:
+        ans = "C"
+        return ans
+    elif letter >= 60:
+        ans = "D"
+        return ans
+    else:
+        ans = "F"
+        return ans
+course_num = int(input("Enter the number of courses you take\n"))
 for course in range(course_num):
     course_name = input("Enter course name\n")
     exam_score = float(input("Enter exam score out of 100\n"))
     ans_out = gpa_1(exam_score)
+    gpa_letter = gpa_2(exam_score)
     print ("Course: ", course_name)
     print ("Exam Score:", exam_score)
     print ("GPA", ans_out)
+    print ("Grade: ", gpa_letter)
