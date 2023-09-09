01e52cf48bdc98016e30a191ef2a320d3126e9e3
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-09-09 20:56:58 +0000

    Calculate average and find maximum value

---
+++ average_n_max.py
@@ -0,0 +1,15 @@
+def calculate_average(numbers):
+    a = sum(numbers)
+    average = sum(numbers) / len(numbers)
+    return average
+def find_maximum(numbers):
+    maximum = max(numbers)
+    return maximum
+numbers = [float(x) for x in input("Enter your list of numbers separating each number with ',' \n").split(",")]
+number_list = f"Your numbers are: {numbers}"
+print(number_list)
+final_average = f"The average is: {calculate_average(numbers)}"
+print(final_average)
+max_value = f"The maximum value is: {find_maximum(numbers)}"
+print(max_value)
+print("GoodBye Now")
