42a19894bbf7ec0f9bc6e6dad8bda41b446f67cf
michaelperryjnr <michaelperryt97@gmail.com>
2024-02-29 14:00:34 +0000

    Added custom user ranges

--- PrimeNumbers.py
+++ PrimeNumbers.py
@@ -1,7 +1,11 @@
 #create an empty array for  prime numbers
+primeNumsStart = int(input("Enter the start of your prime numbers range \n"))
+primeNumsEnd = int(input("Enter the end of your prime numbers range \n"))
+
+
 primeNumbers = []
 #code to generate numbers from 2 to 98
-for x in range(2,98):
+for x in range(primeNumsStart,primeNumsEnd):
     #this code checks if the number is actually a primenumber
     for n in range(2,x):
         if x % n == 0:
@@ -9,5 +13,8 @@ for x in range(2,98):
     else:
         primeNumbers.append(x)
 #the prime numbers are printed and summed with the code below
+primeNumbers.remove(2)
 print(primeNumbers)
+numberOfPrimes = f"The number of prime numbers between {primeNumsStart} and {primeNumsEnd} is {len(primeNumbers)}"
+print(numberOfPrimes)
 print("The sum of Prime Numbers are", sum(primeNumbers))
