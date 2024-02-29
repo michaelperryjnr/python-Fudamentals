2a60f64e4d62a1cf69dbc85adf527301652f272f
michaelperryjnr <michaelperryt97@gmail.com>
2024-02-29 14:00:05 +0000

    Added lines for readability

--- leapYearV2.py
+++ leapYearV2.py
@@ -1,6 +1,10 @@
 #find leaps years including end of centuries 
 #allow user to input year
+
+
 leapYear = int(input("Enter a year \n"))
+
+
 #function to check end of centuries and normal years and to check if they are leap years 
 if leapYear % 100 == 0 and leapYear % 400 == 0:
     print(leapYear, "is an end of century leap year")
